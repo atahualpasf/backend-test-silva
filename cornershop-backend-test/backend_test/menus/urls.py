7e65c65ded68bfe3d880c937dc5c46f448f9c1a3
Atahualpa Silva F <atahualpasilva@gmail.com>
2021-08-08 18:42:36 -0400

    feat(menus): adding link to detail in menu list

--- cornershop-backend-test/backend_test/menus/urls.py
+++ cornershop-backend-test/backend_test/menus/urls.py
@@ -8,7 +8,7 @@ from backend_test.menus import views
 
 urlpatterns = [
     path(route="", view=views.MenusListView.as_view(), name="index"),
-    path(route="<int:pk>", view=views.MenuDetailView.as_view(), name="index"),
+    path(route="<int:pk>", view=views.MenuDetailView.as_view(), name="detail"),
     path(route="coordinate", view=views.coordinate, name="coordinate"),
     path(route="options", view=views.options, name="options"),
 ]
