5823959c533981a7c5fc97d27972567b9b4e0577
Atahualpa Silva F <atahualpasilva@gmail.com>
2021-08-07 23:57:40 -0400

    feat(auth): login and logout on users

---
+++ cornershop-backend-test/backend_test/users/urls.py
@@ -0,0 +1,12 @@
+"""Users urls."""
+
+# Django
+from django.urls import path
+
+# Backend test
+from backend_test.users import views
+
+urlpatterns = [
+    path(route="login", view=views.LoginView.as_view(), name="login"),
+    path(route="logout", view=views.LogoutView.as_view(), name="logout"),
+]
