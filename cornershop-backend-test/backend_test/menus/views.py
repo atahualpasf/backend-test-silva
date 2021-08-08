2881c05f275c484be2db5391920808b9cf6943b0
Atahualpa Silva F <atahualpasilva@gmail.com>
2021-08-08 18:36:59 -0400

    feat(menus): adding menu detail view

--- cornershop-backend-test/backend_test/menus/views.py
+++ cornershop-backend-test/backend_test/menus/views.py
@@ -2,10 +2,10 @@
 
 # Django
 from django.http.response import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 # Backend test
-from backend_test.menus.models import Menu
+from backend_test.menus.models import Menu, MenuOption
 from backend_test.utils.mixins import GroupRequiredMixin
 
 
@@ -22,8 +22,33 @@ class MenusListView(GroupRequiredMixin, ListView):
 
     # GroupRequiredMixin
     group_required = ["meal_coordinator"]
-    # Default
-    template_name = "menus/list.html"
+
+    # ListView default
     model = Menu
     paginate_by = 4
     context_object_name = "menus"
+    template_name = "menus/list.html"
+
+
+class MenuDetailView(GroupRequiredMixin, DetailView):
+    """Return menu detail."""
+
+    # GroupRequiredMixin
+    group_required = ["meal_coordinator"]
+
+    # DetailView default
+    model = Menu
+    context_object_name = "menu"
+    template_name = "menus/detail.html"
+
+    def get_context_data(self, **kwargs):
+        """
+        Passing menu options of the menu in context data.
+        """
+        context = super(MenuDetailView, self).get_context_data(**kwargs)
+        context["menu_options"] = (
+            MenuOption.objects.filter(menu=self.object)
+            .select_related("meal")
+            .order_by("option")
+        )
+        return context
